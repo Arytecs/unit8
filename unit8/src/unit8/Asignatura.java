2199f069884ad2ef1d5790a66b531ebaa400cfd6
Araceli <araceliteruel@gmail.com>
2015-05-03 17:16:49 +0200

    Implementa la clase Asignatura

--- unit8/src/unit8/Asignatura.java
+++ unit8/src/unit8/Asignatura.java
@@ -11,4 +11,27 @@ package unit8;
  */
 public class Asignatura {
     
+    private String nombre;
+    private double nota;
+    public Asignatura(String nombre)
+    {
+        this.nombre=nombre;
+    }
+    public String getNombre()
+    {
+        return nombre;
+    }
+    public void setNombre(String nombre)
+    {
+        this.nombre=nombre;
+    }
+    public double getNota()
+    {
+        return nota;
+    }
+    public void setNota(double nota)
+    {
+        this.nota=nota;
+    }
+    
 }
